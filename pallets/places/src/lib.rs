dd41e0db75c2af7eb5f1b9739626b30bbe0e1dc9
andrevsm <asuarez@fmit-solutions.com>
2023-07-17 20:36:17 +0200

    chore: :truck: re-export pallet_timestamp for bookings

--- pallets/places/src/lib.rs
+++ pallets/places/src/lib.rs
@@ -6,6 +6,8 @@ pub mod functions;
 pub mod interface;
 pub mod structures;
 
+pub use pallet_timestamp;
+
 #[frame_support::pallet(dev_mode)]
 pub mod pallet {
 	use crate::interface::PlacesInterface;
