a12ecca4a1df8e66ce09d1c23ba04d882c137676
andrevsm <asuarez@fmit-solutions.com>
2023-08-15 14:16:52 +0200

    refactor: use chrono for dates management

--- pallets/bookings/tests/lib.rs
+++ pallets/bookings/tests/lib.rs
@@ -1,18 +1,12 @@
 #[cfg(test)]
 pub mod mock;
+pub mod utils;
 use frame_support::{assert_noop, assert_ok};
 use pallet_bookings::{BookingData, BookingState, Error};
 use pallet_places::PlaceType;
 
 use crate::mock::*;
-use sp_core::H256;
-
-fn create_hash(data: &str) -> H256 {
-	let bytes = data.as_bytes();
-	let mut array = [0; 32];
-	array[..bytes.len()].copy_from_slice(bytes);
-	H256::from_slice(&array)
-}
+use crate::utils::*;
 
 fn create_demo_place() {
 	let _ = Places::create_place(
@@ -42,14 +36,22 @@ fn build_with_defult_place() -> sp_io::TestExternalities {
 fn test_modify_timestamp_function() {
 	// Should work with seconds
 	build_with_funded_accounts().execute_with(|| {
-		let start_date = 1696922449;
+		let year = 2025;
+		let month = 4;
+		let start_day = 10;
+		let end_day = 13;
+
 		let checkin_hour = 17;
-		let end_date = 1697354449;
 		let checkout_hour = 12;
 
+		let start_date: u64 = generate_timestamp(year, month, start_day, 17, 33, 44);
+		let end_date: u64 = generate_timestamp(year, month, end_day, 17, 33, 44);
+
 		// Expected times
-		let expected_start_date = 1696957200000;
-		let expected_end_date = 1697371200000;
+		let expected_start_date: u64 =
+			generate_timestamp_millis(year, month, start_day, checkin_hour, 0, 0);
+		let expected_end_date: u64 =
+			generate_timestamp_millis(year, month, end_day, checkout_hour, 0, 0);
 
 		assert_eq!(
 			Bookings::modify_timestamp(start_date, checkin_hour).unwrap(),
@@ -60,14 +62,22 @@ fn test_modify_timestamp_function() {
 
 	// Should work with milliseconds
 	build_with_funded_accounts().execute_with(|| {
-		let start_date = 1696922449000;
+		let year = 2025;
+		let month = 4;
+		let start_day = 10;
+		let end_day = 13;
+
 		let checkin_hour = 17;
-		let end_date = 1697354449000;
 		let checkout_hour = 12;
 
+		let start_date: u64 = generate_timestamp_millis(year, month, start_day, 17, 33, 44);
+		let end_date: u64 = generate_timestamp_millis(year, month, end_day, 17, 33, 44);
+
 		// Expected times
-		let expected_start_date = 1696957200000;
-		let expected_end_date = 1697371200000;
+		let expected_start_date: u64 =
+			generate_timestamp_millis(year, month, start_day, checkin_hour, 0, 0);
+		let expected_end_date: u64 =
+			generate_timestamp_millis(year, month, end_day, checkout_hour, 0, 0);
 
 		assert_eq!(
 			Bookings::modify_timestamp(start_date, checkin_hour).unwrap(),
@@ -77,8 +87,13 @@ fn test_modify_timestamp_function() {
 	});
 
 	build_with_funded_accounts().execute_with(|| {
-		let start_date = 1696922449;
-		let checkin_hour = 25; // Should fail
+		let year = 2025;
+		let month = 4;
+		let start_day = 10;
+
+		let checkin_hour = 25;
+
+		let start_date: u64 = generate_timestamp_millis(year, month, start_day, 17, 33, 44);
 
 		assert_noop!(
 			Bookings::modify_timestamp(start_date, checkin_hour),
@@ -96,8 +111,14 @@ fn test_create_booking_should_work() {
 		let place_id = Places::get_all_places()[0];
 		let place_data = Places::get_place_by_id(place_id).unwrap();
 
-		let start_date = 1696922449;
-		let end_date = 1697354449;
+		let year = 2025;
+		let month = 4;
+		let start_day = 10;
+		let end_day = 13;
+
+		let start_date: u64 = generate_timestamp_millis(year, month, start_day, 17, 33, 44);
+		let end_date: u64 = generate_timestamp_millis(year, month, end_day, 17, 33, 44);
+
 		let amount = 10;
 
 		assert_ok!(Bookings::create_booking(
@@ -137,15 +158,20 @@ fn test_create_booking_with_invalid_dates_should_fail() {
 	build_with_defult_place().execute_with(|| {
 		let place_id = Places::get_all_places()[0];
 
-		let start_date = 1696922449;
-		let end_date = 1697354449;
+		let year = 2025;
+		let month = 4;
+		let start_day = 10;
+		let end_day = 13;
+
+		let start_date: u64 = generate_timestamp_millis(year, month, start_day, 17, 33, 44);
+		let end_date: u64 = generate_timestamp_millis(year, month, end_day, 17, 33, 44);
 		let amount = 10;
 
 		assert_noop!(
 			Bookings::create_booking(
 				RuntimeOrigin::signed(1),
 				place_id,
-				end_date,
+				end_date, // switched dates
 				start_date,
 				amount
 			),
@@ -159,8 +185,13 @@ fn test_create_booking_without_funds_should_fail() {
 	build_with_defult_place().execute_with(|| {
 		let place_id = Places::get_all_places()[0];
 
-		let start_date = 1696922449;
-		let end_date = 1697354449;
+		let year = 2025;
+		let month = 4;
+		let start_day = 10;
+		let end_day = 13;
+
+		let start_date: u64 = generate_timestamp_millis(year, month, start_day, 17, 33, 44);
+		let end_date: u64 = generate_timestamp_millis(year, month, end_day, 17, 33, 44);
 		let amount = 10;
 
 		assert_noop!(
@@ -180,10 +211,42 @@ fn test_create_booking_without_funds_should_fail() {
 fn test_create_booking_in_owned_place_should_fail() {
 	build_with_defult_place().execute_with(|| {
 		let place_id = Places::get_all_places()[0];
+
+		let year = 2025;
+		let month = 4;
+		let start_day = 10;
+		let end_day = 13;
+
+		let start_date: u64 = generate_timestamp_millis(year, month, start_day, 17, 33, 44);
+		let end_date: u64 = generate_timestamp_millis(year, month, end_day, 17, 33, 44);
+		let amount = 10;
+
+		assert_noop!(
+			Bookings::create_booking(
+				RuntimeOrigin::signed(0),
+				place_id,
+				start_date,
+				end_date,
+				amount
+			),
+			Error::<Test>::CannotBookOwnedPlace
+		);
+	})
+}
+
+#[test]
+fn test_create_booking_with_outdated_start_day_should_fail() {
+	build_with_defult_place().execute_with(|| {
+		let place_id = Places::get_all_places()[0];
 		let place_data = Places::get_place_by_id(place_id).unwrap();
 
-		let start_date = 1696922449;
-		let end_date = 1697354449;
+		let year = 2025;
+		let month = 4;
+		let start_day = 10;
+		let end_day = 13;
+
+		let start_date: u64 = generate_timestamp_millis(year, month, start_day, 17, 33, 44);
+		let end_date: u64 = generate_timestamp_millis(year, month, end_day, 17, 33, 44);
 		let amount = 10;
 
 		let current_time = Bookings::modify_timestamp(start_date, place_data.checkin_hour).unwrap();
